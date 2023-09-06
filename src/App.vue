c44d2d59165eb41b31e9829a0e2f2c9b32ae3775
TNZ <92765882+tehau26nz@users.noreply.github.com>
2023-09-07 10:39:16 +1200

    Score display

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
  <h1>Ninja Reaction Timer</h1>
  <button @click="start" :disabled="isPlaying">Play</button>
  <Block v-if="isPlaying" :delay="delay" @end="endGame"/>
- <p>Reaction time: {{ score }} ms</p>
+ <p v-if="showResults">Reaction time: {{ score }} ms</p>
 </template>
 
 <script>
@@ -15,7 +15,8 @@ export default {
     return{
       isPlaying: false,
       delay: null,
-      score: null
+      score: null,
+      showResults: false
     }
 
   },
@@ -23,10 +24,12 @@ export default {
     start(){
       this.delay = 2000 + Math.random() * 5000
       this.isPlaying = true
+      this.showResults = false
     },
     endGame(reactionTime){
       this.score = reactionTime
       this.isPlaying = false
+      this.showResults = true
     }
   }
 }
